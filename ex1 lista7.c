5a3d79f3f29f9073b9dfaa35020c27fd4afb90e1
EduardoAlvim <105020093+EduardoAlvim@users.noreply.github.com>
2022-06-23 17:59:32 -0300

    Update ex1 lista7.c

--- ex1 lista7.c
+++ ex1 lista7.c
@@ -2,17 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*Escreva um algoritmo que solicite ao usuário uma sequência de caracteres e
-realize as seguintes operações, utilizando as estruturas de dados pilha e fila:
+/*Escreva um algoritmo que solicite ao usuÃ¡rio uma sequÃªncia de caracteres e
+realize as seguintes operaÃ§Ãµes, utilizando as estruturas de dados pilha e fila:
 a) Imprimir o texto na ordem inversa;
-b) Verificar se o texto é um palíndromo, ou seja, se a string é escrita da mesma
-maneira de frente para trás e de trás para frente. Ignore espaços e pontos.*/
+b) Verificar se o texto Ã© um palÃ­ndromo, ou seja, se a string Ã© escrita da mesma
+maneira de frente para trÃ¡s e de trÃ¡s para frente. Ignore espaÃ§os e pontos.*/
+
+
+typedef struct
+{
+    char item[100];
+    int inicio,fim;
+} Fila;
 
 struct stack
 {
     int topo;
-    int dados[100];
+    char dados[100];
 };
+
+//-----------------Fila----------------------------
+
+void Inicia(Fila *F)
+{
+    F->inicio=F->fim=0;
+}
+
+int Insere(Fila *F,char X)
+{
+    if(F->fim==100)
+        return 0;
+    else
+    {
+        F->item[F->fim]=X;
+        F->fim=F->fim+1;
+        return 1;
+    }
+}
+
+int Remove(Fila *F,char *X)
+{
+    if(F->inicio==F->fim)
+        return 0;
+    else
+    {
+        *X=F->item[F->inicio];
+        F->inicio=F->inicio+1;
+        return 1;
+    }
+}
+
+//-----------------Pilha-----------------------------
 
 void empilha (char x, struct stack *pilha)
 {
@@ -42,30 +82,49 @@ char desempilha(struct stack *pilha)
     }
 }
 
+
 void main()
 {
+    Fila fila;
+    Inicia(&fila);
     struct stack pilha;
     pilha.topo=-1;
     char palavra[100],invertida[100];
+    int tam, i;
     printf("\nDigite a frase: ");
     gets(palavra);
     fflush(stdin);
-    for(int i=0; i<strlen(palavra); i++)
+    tam=strlen(palavra);
+    for(i=0; i<tam; i++)
+    {
+        empilha(palavra[i],&pilha);
+        Insere(&fila,palavra[i]);
+    }
+    for(i=0;i<tam;i++){
+        invertida[i]=desempilha(&pilha);
+    }
+    invertida[i]='\0';
+    for(i=0; i<tam; i++)
     {
         empilha(palavra[i],&pilha);
     }
-    for(int i=0; i<strlen(palavra); i++)
-    {
-        invertida[i]=desempilha(&pilha);
-    }
-    printf("\nFrase normal: %s\nFrase invertida: %s",palavra,invertida);
 
-    if(strcmp(palavra,invertida)==0)
+    printf("\nFrase normal: %s\nFrase invertida: %s",palavra,invertida);
+    int cont=0;
+    for(i=0; i<tam; i++)
     {
-        printf("\nA frase e um palindromo");
+        char in, f;
+        Remove(&fila,&in);
+        f=desempilha(&pilha);
+        if(in != f)
+        {
+            cont++;
+        }
     }
-    else
-    {
-        printf("\nA frase nao e um palindromo");
+    if(cont > 0){
+        printf("\nA palavra nao e um palindromo");
+    }
+    else{
+        printf("\nA palavra e um palindromo");
     }
 }
